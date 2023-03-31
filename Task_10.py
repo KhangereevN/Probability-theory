9b328a0329705381c5016ca9348ea4601cda6104
KhangereevN <111253050+KhangereevN@users.noreply.github.com>
2023-03-31 08:51:54 +0300

    Update Task_10.py

--- Task_10.py
+++ Task_10.py
@@ -7,7 +7,7 @@ w_data = np.array([172, 173, 169, 177, 166, 180, 178, 177, 172, 166, 170])
 
 f, p_value = stats.f_oneway(f_data, h_data, w_data)
 print('p_value =', round((p_value),4))
-print('f_value =', round((f),4))
+print('F_статистика =', round((f),4))
 if p_value < 0.05:
     print("Средний рост различается в зависимости от вида спорта (гипотеза H1)")
 else:
